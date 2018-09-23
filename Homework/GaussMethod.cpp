de1ba8f5a3c52ecdbd7661fb3a97ac195f7eb140
NyakaNyasha <43213564+NyakaNyasha@users.noreply.github.com>
2018-09-23 23:18:59 +0300

    Create GaussMethod.cpp

---
+++ Homework/GaussMethod.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    double x
+    cin>>n;
+    int a[n][n+1];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+1;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int k=0;k<n-1;k++) {
+        for (int i = 1; i < n; i++) {
+            int c = 0 - a[i][k] / a[k][k];
+            for (int j = 0; j < i; j++) {
+                a[i][j] = c * a[k][j] + a[i][j];
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+1;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
