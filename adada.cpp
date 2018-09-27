7d0e51324cf3668d86fb0d05abb08799f3012c6c
NyakaNyasha <43213564+NyakaNyasha@users.noreply.github.com>
2018-09-27 18:55:45 +0300

    Create adada.cpp

---
+++ adada.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int i, int s, int* & a){
+    for (i=0; i<s; i++){
+        for (int j=0; j<s; j++){
+            if ((a[i]==a[j]) && (i != j)) {
+                for ( int q=j; q<s-1; q++){
+                    a[q]=a[q+1];}
+                s--;
+            }
+        }
+    }
+    /*int b = new [s];
+    for (i=0; i<s; i++){
+        b[i] = a[i];
+    }
+    delete []a;
+    a=b;
+    }
+    */
+    for (i=0; i<s; i++){
+        cout << a[i] << ' ';
+    }
+}
+
+int main()
+{
+int s,i;
+int a[s];
+    cin >> s;
+    for (i=0; i<s; i++){
+        cin >> a[i];
+    }
+    fun(i,s,a);
+    return 0;
+}
