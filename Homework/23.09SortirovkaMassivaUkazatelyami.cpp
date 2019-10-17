231954d2708f7bd5288153f664f889f34564e710
NyakaNyasha <43213564+NyakaNyasha@users.noreply.github.com>
2019-10-17 13:16:45 +0300

    Update 23.09SortirovkaMassivaUkazatelyami.cpp

--- Homework/23.09SortirovkaMassivaUkazatelyami.cpp
+++ Homework/23.09SortirovkaMassivaUkazatelyami.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 int main() {
-	int r, i, s, j;        // i и j для прокрутки циклов
-	s = 10;                   // r - дополнительное ведро когда меняем местами
+	int r, i, s, j;       
+	s = 10;                   
 	int a[s];
-	for (i = 0; i < s; i = i + 1) {     //вводим массив
+	for (i = 0; i < s; i = i + 1) {    
 		cin >> a[i];
 	}
-	int *ptr = a;            // тут объявили указатель
-	for (i = 0; i < s; i = i + 1) {     //начинаем крутить циклы
+	int *ptr = a;         
+	for (i = 0; i < s; i = i + 1) {  
 		for (j = 0; j < s; j = j + 1) {
 			if (*(ptr + j) > *(ptr + i)) {
-				r = *(ptr + j);       // меняем местами
+				r = *(ptr + j);      
 				*(ptr + j) = *(ptr + i);
 				*(ptr + i) = r;
 			}
 		}
 	}
-	for (i = 0; i < s; i = i + 1) {       //выводим массив
+	for (i = 0; i < s; i = i + 1) {    
 		cout << *(ptr + i) << ' ';
 	}
 	return 0;
